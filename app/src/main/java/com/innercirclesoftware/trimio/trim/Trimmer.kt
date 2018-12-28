2e94392f06167a47796830f86272378fe5995e9d
Maciej Kaznowski <mmkaznowski@gmail.com>
2018-12-28 12:24:55 +0100

    Add basic trim implementation

--- app/src/main/java/com/innercirclesoftware/trimio/trim/Trimmer.kt
+++ app/src/main/java/com/innercirclesoftware/trimio/trim/Trimmer.kt
@@ -1,20 +1,34 @@
 package com.innercirclesoftware.trimio.trim
 
+import com.github.ajalt.timberkt.Timber
+import eu.chainfire.libsuperuser.Shell
 import io.reactivex.Completable
-import java.util.concurrent.TimeUnit
+import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
 
 interface Trimmer {
 
     //TODO convert to Observable<Progress>
     fun trim(partition: Partition): Completable
 
+    fun trimAll(): Completable {
+        val trimCache = trim(Partition.Cache)
+        val trimData = trim(Partition.Data)
+        val trimSystem = trim(Partition.System)
+
+        return Completable.concat(listOf(trimCache, trimData, trimSystem))
+    }
 }
 
 class TrimmerImpl : Trimmer {
 
-    //TODO implement trimming
     override fun trim(partition: Partition): Completable {
-        return Completable.timer(5, TimeUnit.SECONDS)
+        return Single
+            .fromCallable { "fstrim -v ${partition.directory}" }
+            .subscribeOn(Schedulers.io()) //TODO determine which Thread to use
+            .doOnSuccess { Timber.v { "Executing \"$it\"" } }
+            .map { Shell.SU.run(it) }
+            .doOnSuccess { Timber.v { "Finished. Result=\"$it\"" } }
+            .ignoreElement()
     }
-
 }
